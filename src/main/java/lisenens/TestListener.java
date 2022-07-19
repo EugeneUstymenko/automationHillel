385d281136ee260a3ca43b4c71edcc888bf7da0d
Evgenii Ustimenko <u.evgenii7@gmail.com>
2022-07-19 19:19:38 +0300

    add fixnew

--- src/main/java/lisenens/TestListener.java
+++ src/main/java/lisenens/TestListener.java
@@ -1,15 +1,9 @@
 package lisenens;
 
-import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class TestListener implements ITestListener {
-
-    @Override
-    public void onTestStart(ITestResult result) {
-        ITestListener.super.onTestStart(result);
-    }
 
     @Override
     public void onTestSuccess(ITestResult result) {
@@ -21,28 +15,4 @@ public class TestListener implements ITestListener {
         System.out.println("TEST IS FAILED" + result.getMethod());
     }
 
-    @Override
-    public void onTestSkipped(ITestResult result) {
-        ITestListener.super.onTestSkipped(result);
-    }
-
-    @Override
-    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-        ITestListener.super.onTestFailedButWithinSuccessPercentage(result);
-    }
-
-    @Override
-    public void onTestFailedWithTimeout(ITestResult result) {
-        ITestListener.super.onTestFailedWithTimeout(result);
-    }
-
-    @Override
-    public void onStart(ITestContext context) {
-        ITestListener.super.onStart(context);
-    }
-
-    @Override
-    public void onFinish(ITestContext context) {
-        ITestListener.super.onFinish(context);
-    }
 }
